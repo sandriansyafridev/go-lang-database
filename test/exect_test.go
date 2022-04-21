7b8423239da632c21acb3d47f1378a3c7c37a848
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-22 01:04:51 +0700

    read data from database

--- test/exect_test.go
+++ test/exect_test.go
@@ -3,30 +3,53 @@ package test
 import (
 	"context"
 	"go-lang-database/db"
+	"log"
 	"testing"
 )
 
 //exect =  tidak mengembalian nilai
 //query  = mengembalikan nilai
-func TestExecContextInsertDatabase(t *testing.T) {
 
+type User struct {
+	ID    int
+	Name  string
+	Email string
+}
+
+func TestQueryContextSelect(t *testing.T) {
+
+	//setup database
 	db, _ := db.SetupDatabase()
 
+	//create context
 	ctx := context.Background()
-	query := "INSERT INTO users VALUES(0,?,?)"
+
+	//create query
+	query := "SELECT * FROM users"
 	stmt, err := db.Prepare(query)
 	if err != nil {
 		t.Error("Failed to prepare data: ", err.Error())
 	}
-
-	user := map[string]string{
-		"name":  "Sandrian",
-		"email": "sandrian@gmail.com",
-	}
-
-	_, err = stmt.ExecContext(ctx, user["name"], user["email"])
+	rows, err := stmt.QueryContext(ctx)
 	if err != nil {
 		t.Error("Failed to insert data: ", err.Error())
+	}
+
+	//process scanning data
+	users := []User{}
+	defer rows.Close()
+	for rows.Next() {
+		user := User{}
+		err := rows.Scan(&user.ID, &user.Name, &user.Email)
+		if err != nil {
+			t.Error("Faild scan data:", err.Error())
+		}
+		users = append(users, user)
+	}
+
+	// log result.
+	for _, user := range users {
+		log.Println(user)
 	}
 
 }
