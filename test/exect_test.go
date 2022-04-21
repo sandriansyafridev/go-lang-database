d449e040a920e0e60ecbc6bbc831871d89d4eb97
sandriansyafridev <sandriansyafri.dev@gmail.com>
2022-04-22 01:10:06 +0700

    create data with many type data

--- test/exect_test.go
+++ test/exect_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"go-lang-database/db"
 	"testing"
+	"time"
 )
 
 //exect =  tidak mengembalian nilai
@@ -13,18 +14,21 @@ func TestExecContextInsertDatabase(t *testing.T) {
 	db, _ := db.SetupDatabase()
 
 	ctx := context.Background()
-	query := "INSERT INTO users VALUES(0,?,?)"
+	query := "INSERT INTO users VALUES(0,?,?,?,?,?)"
 	stmt, err := db.Prepare(query)
 	if err != nil {
 		t.Error("Failed to prepare data: ", err.Error())
 	}
 
-	user := map[string]string{
-		"name":  "Sandrian",
-		"email": "sandrian@gmail.com",
+	user := map[string]interface{}{
+		"name":       "Hafid",
+		"email":      "hafid@gmail.com",
+		"age":        24,
+		"is_student": false,
+		"created_at": time.Now(),
 	}
 
-	_, err = stmt.ExecContext(ctx, user["name"], user["email"])
+	_, err = stmt.ExecContext(ctx, user["name"], user["email"], user["age"], user["is_student"], user["created_at"])
 	if err != nil {
 		t.Error("Failed to insert data: ", err.Error())
 	}
